97f15d7ec9d8f0d75a3972d57491d680d9b067d1
lkgongjake <lkgongjake@gmail.com>
2019-02-13 20:34:49 +0800

    Some optimization

---
+++ README.md
@@ -0,0 +1,12 @@
+# Alertmanager Webhook
+
+接收Alertmanager发送的请求，通过[Pushbear](http://pushbear.ftqq.com/admin/#/)将告警消息推送到微信，并记录日志。
+
+感谢[Easy](https://github.com/easychen)提供这么好的服务。
+
+##　使用
+
+1. 注册Pushbear，得到通道的sendKey；
+1. 填入`sendKey`变量中；
+1. 编译：`go build -o alertwebhook main.go`；
+1. 赋予执行权限，直接运行；
--- main.go
+++ main.go
@@ -1,60 +1,143 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
-	)
-
-var db = make(map[string]string)
+	"net/url"
+	"os"
+)
 
 type labels struct {
-	AlertName	string	`json:"alertname"`
-	Instance 	string	`json:"instance"`
-	Job 		string	`json:"job"`
-	Servity 	string	`json:"servity"`
+	AlertName string `json:"alertname"`
+	Instance  string `json:"instance"`
+	Job       string `json:"job"`
+	Servity   string `json:"servity"`
 }
 
 type annotations struct {
-	Description 	string 	`json:"description"`
-	Summary 		string 	`json:"summary"`
+	Description string `json:"description"`
+	Summary     string `json:"summary"`
 }
 
 type alerts struct {
-	Status 			string			`json:"status"`
-	Labels 			labels 			`json:"labels"`
-	Annotations 	annotations 	`json:"annotations"`
-	StartsAt 		string 			`json:"startsAt"`
-	EndsAt 			string			`json:"endsAt"`
+	Status      string      `json:"status"`
+	Labels      labels      `json:"labels"`
+	Annotations annotations `json:"annotations"`
+	StartsAt    string      `json:"startsAt"`
+	EndsAt      string      `json:"endsAt"`
 }
 
-
-type alertMessage struct {
-	Alert 	[]alerts 	`json:"alerts"`
+type alertMessages struct {
+	Alert []alerts `json:"alerts"`
 }
 
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
+type RespBodyStruct struct {
+	Code    int    `json:"code, string"`
+	Message string `json:"message"`
+	// omitempty：在序列化的时候忽略零值或者空值
+	Data    string `json:"data, omitempty"`
+	Created string `json:"created"`
+}
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+var (
+	logFile = "/var/log/alertwebhook.log"
+	sendKey = "xxxx-12ec757194444ca97236c686510a4126"
+)
 
-	r.POST("/alert", func(c *gin.Context) {
-		AlertMess := alertMessage{}
-		c.BindJSON(&AlertMess)
-		var Status string = AlertMess.Alert[0].Status
-		c.JSON(http.StatusOK, gin.H{"status": Status})
-	})
+func handler(w http.ResponseWriter, r *http.Request) {
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("err opening file: %v\n", err)
+	}
+	defer f.Close()
 
-	return r
+	log.SetOutput(f)
+
+	decoder := json.NewDecoder(r.Body)
+	alertMess := alertMessages{}
+
+	err = decoder.Decode(&alertMess)
+	if err != nil {
+		panic(err)
+	}
+
+	var alertString string
+
+	for i := 0; i < len(alertMess.Alert); i++ {
+		log.Printf("告警内容：" + alertMess.Alert[i].Status + " " +
+			alertMess.Alert[i].Labels.AlertName + " " +
+			alertMess.Alert[i].Labels.Job + " " +
+			alertMess.Alert[i].Labels.Instance + " " +
+			alertMess.Alert[i].Annotations.Summary,
+		)
+		alertString = alertString + "\n\n" + "**" + alertMess.Alert[i].Labels.AlertName + "**" + "\n\n" +
+			alertMess.Alert[i].Status + "\n\n" +
+			alertMess.Alert[i].Labels.Job + "\n\n" +
+			alertMess.Alert[i].Labels.Instance + "\n\n" +
+			alertMess.Alert[i].Annotations.Description + "\n\n" +
+			alertMess.Alert[i].Annotations.Summary
+	}
+
+	title := "来告警信息了"
+
+	status, err := sendMessage(title, alertString)
+	if err != nil {
+		log.Printf(err.Error() + "\n")
+	}
+	if status == true {
+		log.Printf("告警信息推送成功\n")
+	}
+
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong\n")
+}
+
+// 传入发送标题和内容，返回bool类型和err信息，为true表示发送成功，false表示发送失败
+func sendMessage(title, message string) (status bool, err error) {
+
+	postUrl := "https://pushbear.ftqq.com/sub"
+
+	parameters := url.Values{}
+	parameters.Add("sendkey", sendKey)
+	parameters.Add("text", title)
+	parameters.Add("desp", message)
+
+	resp, err := http.PostForm(postUrl, parameters)
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	respBD := &RespBodyStruct{}
+
+	err = json.Unmarshal(body, respBD)
+	if err != nil {
+		return false, err
+	}
+
+	if respBD.Code != 0 {
+		return false, errors.New(respBD.Message)
+	}
+
+	return true, nil
+
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:9999
-	r.Run(":9999")
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/alert", handler)
+	log.Printf("Start to listening the incoming requests on http address: %s", ":9999")
+	log.Fatal(http.ListenAndServe(":9999", nil))
 }
